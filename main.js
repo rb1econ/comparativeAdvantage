38e475dff571562235cd0927032830af1feec7cb
Ryan budny <rb1econ@gmail.com>
2015-02-12 09:13:01 -0700

    numbers all work

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 $(document).on('ready', function() {
 
 // variables that may or may not be in use::::::::::
-   var goodX = 'cannabis';
+   var goodX = 'Macbooks';
    var goodY = '4Runners';
 
    var wayne = 'Wayne';
    var garth = 'Garth';
 
-   var wayneX = 8;
-   var wayneY = 4;
+   var wayneX = 3;
+   var wayneY = 2;
 
-   var garthX = 4;
-   var garthY = 4;
+   var garthX = 9;
+   var garthY = 5;
 
    var compAdvWayneX=false;
    var compAdvWayneY=false;
@@ -26,41 +26,32 @@ $(document).on('ready', function() {
    var postTradeTotalX=0;
    var postTradeTotalY=0;
 
+   var greaterGoodX = 0;
+   var greaterGoodY = 0;
+
+   var fixedTotalX = 0;
+   var fixedTotalY = 0;
+
+   var postSpecTotalX = 0;
+   var postSpecTotalY = 0;
+
+   var tradeOffer = 0;
+   var tradeRequest = 0;
+
+   var counterOffer=0;
+   var counterRequest=0;
 
    // fill with sample values::::::::
-   $('#sampleBtn').on('click', function(){
-      // wayne='';
-      // garth='';
-      // wayneX='';
-      // wayneY='';
-      
-
-      everything();
-   });
-
-
-
-   // USER INPUT::::::::::::
+   // $('#sampleBtn').on('click', function(){
    
-   
-   $('#submit').on('click', function(event){
-      event.preventDefault();
-      wayne = $('#inputPerson1').val();
-      garth = $('#inputPerson2').val();
-      goodX = $('#inputGood1').val();
-      goodY = $('#inputGood2').val();
-      wayneX = $('#inputGood1Person1').val();
-      wayneY = $('#inputGood2Person1').val();
-      garthX = $('#inputGood1Person2').val();
-      garthY = $('#inputGood2Person2').val();
-      
-      everything();
-   });
 
-   // var wayneAutarkyy = 'Wayne consumes ' + wayneX/2 + ' units of ' + goodX + ' and '+ wayneY/2 + ' of '+ goodY;
-   // var garthAutarky = 'Garth consumes ' + garthX/2 + ' units of ' + goodX + ' and '+ garthY/2 + ' of '+ goodY;
+
+   //    everything();
+   // });
 
    var everything = function(){
+
+      // series of story-like console.logs::::::::::
       var autarkyConsBundle = function(){
          var printOutWayne = wayne + ' consumes ' + wayneX/2 + ' units of ' + goodX + ' and '+ wayneY/2 + ' of '+ goodY+' before trade. ';
          var printOutGarth = garth + ' consumes ' + garthX/2 + ' units of ' + goodX + ' and '+ garthY/2 + ' of '+ goodY + ' before trade. ';
@@ -85,24 +76,32 @@ $(document).on('ready', function() {
                var printOut = wayne+' can produce '+goodX + ' at the lowest cost and '+garth+' can produce '+ goodY+ ' at the lowest cost';
                compAdvWayneX=true; 
                compAdvGarthY=true;
+               compAdvWayneY=false;
+               compAdvGarthX=false;
                return printOut;
             }
             else if(wayneY>wayneX && wayneY>garthY){
                var printOut = garth+' can produce '+goodX + ' at the lowest cost and '+wayne+' can produce '+ goodY+ ' at the lowest cost';
                compAdvWayneY=true;
                compAdvGarthX=true;
+               compAdvGarthY=true;
+               compAdvWayneX=false;
                return printOut;
             }
             else if(garthX>garthY && garthX>wayneX){
                var printOut = garth+' can produce '+goodX + ' at the lowest cost and '+wayne+' can produce '+ goodY+ ' at the lowest cost';
                compAdvWayneY=true;
                compAdvGarthX=true;
+               compAdvGarthY=false;
+               compAdvWayneX=false;
                return printOut;
             }
             else if(garthY>garthX && garthY>wayneY){
                var printOut = wayne+' can produce '+goodX + ' at the lowest cost and '+garth+' can produce '+ goodY+ ' at the lowest cost';
                compAdvWayneX=true; 
                compAdvGarthY=true;
+               compAdvGarthX=false;
+               compAdvWayneY=false;
                return printOut;
             }
          }
@@ -110,12 +109,16 @@ $(document).on('ready', function() {
             var printOut = wayne+' can produce '+goodX + ' at the lowest cost and '+garth+' can produce '+ goodY+ ' at the lowest cost';
             compAdvWayneX=true; 
             compAdvGarthY=true;
+            compAdvWayneY=false;
+            compAdvGarthX=false;
             return printOut;
          }
          else{
             var printOut = garth+' can produce '+goodX + ' at the lowest cost and '+wayne+' can produce '+ goodY+ ' at the lowest cost';
             compAdvWayneY=true;
             compAdvGarthX=true;
+            compAdvGarthY=false;
+            compAdvWayneX=false;
             return printOut;
          }
       };
@@ -124,20 +127,76 @@ $(document).on('ready', function() {
       // compare Totals
       var compareTotals=function(){
          if(compAdvWayneX){
-            postTradeTotalX= wayneX;
-            postTradeTotalY= garthY;
+            postTradeTotalX = wayneX;
+            postSpecTotalX = wayneX;
+
+            postTradeTotalY = garthY;
+            postSpecTotalY = garthY;
+
+            console.log('Wayne produces X: ', postSpecTotalX);
+            console.log('Garth produces Y: ', postSpecTotalY);
          }
          else{
-            postTradeTotalX=garthX;
-            postTradeTotalY=wayneX;
+            postTradeTotalX = garthX;
+            postSpecTotalX = garthX;
+
+            postTradeTotalY = wayneY;
+            postSpecTotalY = wayneY;
+
          }
 
-         var printOut = 'The total amount of '+goodX+' in existence before trade was '+(wayneX/2+garthX/2)+' units. After trade and specialization there are ' + postTradeTotalX +' in existence.';
+         var printOut = 'The total amount of '+goodX+' in existence before trade was '+ (wayneX/2+garthX/2) +' units. After trade and specialization there are '+ postTradeTotalX +' in existence.';
          return printOut;
       };
-
       console.log(compareTotals());
 
+      
+      // Fix Totals if One is less than before:::::
+      
+      var postTradeTotalX = parseInt(postTradeTotalX);
+      var postTradeTotalY = parseInt(postTradeTotalY);
+      // console.log('pre if postTradeTotalX=', postTradeTotalX);
+      // console.log('pre if postTradeTotalY=', postTradeTotalY);
+
+         if(postTradeTotalX<(wayneX/2+garthX/2)){
+            if(compAdvWayneX){   
+               while (postTradeTotalX<wayneX/2+garthX/2){
+                  postTradeTotalY = postTradeTotalY-0.5;
+                  postTradeTotalX = (0.5*(garthX/garthY)) + postTradeTotalX;
+                  // console.log('if postTradeTotalX=', postTradeTotalX);
+                  // console.log('if postTradeTotalY=', postTradeTotalY);
+               }
+            }
+            else{
+               while(postTradeTotalX<wayneX/2+garthX/2){
+                  postTradeTotalY = postTradeTotalY-0.5;
+                  postTradeTotalX = (0.5*(wayneX/wayneY)) + postTradeTotalX;
+                  // console.log('if postTradeTotalX=', postTradeTotalX);
+                  // console.log('if postTradeTotalY=', postTradeTotalY);
+               }
+            }
+         }
+         if(postTradeTotalY<wayneY/2+garthY/2){
+            if(compAdvWayneX){
+               while(postTradeTotalY<wayneY/2+garthY/2){
+                  postTradeTotalX=postTradeTotalX-0.5;
+                  postTradeTotalY=(0.5*(wayneY/wayneX)) + postTradeTotalY;
+                  // console.log('if postTradeTotalX=', postTradeTotalX);
+                  // console.log('if postTradeTotalY=', postTradeTotalY);
+               }
+            }
+            else{
+               while(postTradeTotalY<wayneY/2+garthY/2){
+                  postTradeTotalX=postTradeTotalX-0.5;
+                  postTradeTotalY=(0.5*(garthY/garthX)) + postTradeTotalY;
+                  // console.log('if postTradeTotalX=', postTradeTotalX);
+                  // console.log('if postTradeTotalY=', postTradeTotalY);
+               }
+            }
+         }
+
+      // console.log('after if postTradeTotalX=', postTradeTotalX);
+      // console.log('after if postTradeTotalY=', postTradeTotalY);
       // Terms of Trade:::::
       var termsOfTrade = function(){
          if(compAdvWayneX){
@@ -155,36 +214,163 @@ $(document).on('ready', function() {
       };
       console.log(termsOfTrade());
 
-
-      // append sample case:::::::::::
+      // append variables/sample case:::::::::::
       var introInfo = '<p>We live in a fictitious world with only two goods. They are ' +goodX+' and '+goodY+'. There are only two people; you, '+wayne+', and a potential trading partner, '+garth+'.';
 
-      $('.sampleCase').prepend(introInfo);
+      $('.introSentence').html(introInfo);
 
-      $('#goodX').append(goodX+' production per week if all time devoted to it');
-      $('#goodY').append(goodY+' production per week if all time devoted to it');
-      $('#person1').append('Person 1: '+wayne);
-      $('#goodXperson1').append(wayneX);
-      $('#goodXperson1opCost').append('1 unit of '+goodX+' for ' +wayneY/wayneX+' of '+goodY);
-      $('#goodYperson1').append(wayneY);
-      $('#goodYperson1opCost').append('1 unit of '+goodY+' for ' +wayneX/wayneY+' of '+goodX);
+      // MAIN TABLE
+      $('#goodX').html(goodX);
+      $('#goodY').html(goodY);
+      $('#wayne').html(wayne);
+      $('#goodXwayne').html(wayneX);
+      $('#goodXwayneopCost').html('1 unit of '+goodX+' for ' +wayneY/wayneX+' of '+goodY);
+      $('#goodYwayne').html(wayneY);
+      $('#goodYwayneopCost').html('1 unit of '+goodY+' for ' +wayneX/wayneY+' of '+goodX);
 
-      $('#person2').append('Person 2: '+garth);
-      $('#goodXperson2').append(garthX);
-      $('#goodXperson2opCost').append('1 unit of '+goodX+' for ' +garthY/garthX+' of '+goodY);
-      $('#goodYperson2').append(garthY);
-      $('#goodYperson2opCost').append('1 unit of '+goodY+' for ' +garthX/garthY+' of '+goodX);
+      $('#garth').html(garth);
+      $('#goodXgarth').html(garthX);
+      $('#goodXgarthopCost').html('1 unit of '+goodX+' for ' +garthY/garthX+' of '+goodY);
+      $('#goodYgarth').html(garthY);
+      $('#goodYgarthopCost').html('1 unit of '+goodY+' for ' +garthX/garthY+' of '+goodX);
 
       
       // Table of Totals
-      $('#goodXname').append(goodX);
-      $('#goodYname').append(goodY);
-      $('#totalXbefore').append(wayneX/2+garthX/2);
-      $('#totalXafter').append(postTradeTotalX);
-      $('#totalYbefore').append(wayneY/2+garthY/2);
-      $('#totalYafter').append(postTradeTotalY);
+      
+      $('#totalXbefore').html(wayneX/2+garthX/2);
+      $('#totalXafter').html(postTradeTotalX);
+      $('#totalYbefore').html(wayneY/2+garthY/2);
+      $('#totalYafter').html(postTradeTotalY);
+      $('#totalXspecial').html(postSpecTotalX);
+      $('#totalYspecial').html(postSpecTotalY);
 
+      // append variables to classes
+      $('.goodX').html(goodX);
+      $('.goodY').html(goodY);
+      $('.wayne').html(wayne);
+      $('.garth').html(garth);
+      $('.wayneXbefore').html(wayneX/2);
+      $('.wayneYbefore').html(wayneY/2);
+      $('.garthXbefore').html(garthX/2);
+      $('.garthYbefore').html(garthY/2);
 
+      // trade table
+      $('#offer').html(tradeOffer);
+      $('#request').html(tradeRequest);
 
+      // Chartzzzzzzzzzzzzz
+
+     $(function () {
+         $('#chartEvery').highcharts({
+             chart: {
+                 type: 'column'
+             },
+             title: {
+                 text: 'Before and After Trade'
+             },
+             subtitle: {
+                 text: 'Demonstration of Comparative Advantage'
+             },
+             xAxis: {
+                 categories: [
+                     'Before Trade Totals',
+                     'After Trade Totals',
+                     ''+wayne+' Before Trade',
+                     ''+garth +' Before Trade',
+                     ''+wayne+' After Trade(disregard)',
+                     ''+garth +'After Trade(disregard)'    
+                 ]
+             },
+             yAxis: {
+                 min: 0,
+                 title: {
+                     text: 'Units of the two Goods'
+                 }
+             },
+             tooltip: {
+                 headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                     '<td style="padding:0"><b>{point.y:.1f} units</b></td></tr>',
+                 footerFormat: '</table>',
+                 shared: true,
+                 useHTML: true
+             },
+             plotOptions: {
+                 column: {
+                     pointPadding: 0.2,
+                     borderWidth: 0
+                 }
+             },
+             series: [{
+                 name: goodX,
+                 data: [wayneX/2+garthX/2, postTradeTotalX, wayneX/2, garthX/2, 2, 2]
+
+             }, {
+                 name: goodY,
+                 data: [wayneY/2+garthY/2, postTradeTotalY, wayneY/2, garthY/2, 2, 2]
+
+             }]
+         });
+     });
    };
+   // This bracket closes everything();
+   
+// Edit in place javascript::::::
+      var saveBtn = '';
+      var closeBtn = '';
+      var inputInfo = '';
+      $('.editable').on('click', function(){
+          $(this).hide();
+          inputInfo = $('<textarea rows="1" cols="7" class="info">');
+          $(this).after(inputInfo);
+          closeBtn = $('<button class="close btn btn-default">X</button>');
+          $(inputInfo).after(closeBtn);
+          saveBtn = $('<button class="save btn btn-default">✓</button>');
+          $(inputInfo).after(saveBtn);
+          $('textarea').focus();
+      });
+
+      $('body').on('click', '.close', function(){
+          
+          $(this).parent().children('.info').remove();
+          $(this).parent().children('.editable').show();
+          $(this).parent().children('.save').remove();
+          $(this).parent().children('.close').remove();
+      });
+
+
+      $('body').on('click', '.save', function(){
+          
+         var userInput = $(this).siblings('textarea').val();
+          // console.log(userInput);
+          
+
+          // set variables to their corresponding table elements:::::THERE ARE 8 IN TOTAL:::::::
+         wayne = $('#wayne').siblings('textarea').val() || $('#wayne').text();
+         garth = $('#garth').siblings('textarea').val() || $('#garth').text();
+
+         goodX =$('#goodX').siblings('textarea').val() || $('#goodX').text();
+         goodY = $('#goodY').siblings('textarea').val() || $('#goodY').text();
+
+         wayneX=$('#goodXwayne').siblings('textarea').val() || $('#goodXwayne').text();
+         wayneY=$('#goodYwayne').siblings('textarea').val() || $('#goodYwayne').text();
+
+         garthX=$('#goodXgarth').siblings('textarea').val() || $('#goodXgarth').text();
+         garthY=$('#goodYgarth').siblings('textarea').val() || $('#goodYgarth').text();
+
+         tradeOffer=$('#offer').siblings('textarea').val() || $('#tradeOffer').text();
+         tradeRequest=$('#request').siblings('textarea').val() || $('#tradeRequest').text();
+         everything();
+
+         // $(this).parent().find('p').html(userInput);
+
+         $(this).parent().children('.info').remove();
+         $(this).parent().children('.editable').show();
+         $(this).parent().children('.close').remove();
+         $(this).parent().children('.save').remove();
+
+      });
+
+
+   everything();
 });
